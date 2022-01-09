a66193212bcbeff7adc094bdabcb6315a04abb1c
Roman Krajewski <s0553506@htw-berlin.de>
2022-01-09 14:29:02 +0100

    implement movement

--- src/components/BabylonCanvas.vue
+++ src/components/BabylonCanvas.vue
@@ -1,5 +1,8 @@
 <template>
-<canvas ref = 'canvas' ></canvas>
+  <div>
+    <canvas ref = 'canvas' ></canvas>
+    <p>{{debug_message}}</p>
+  </div>
 </template>
 
 <script>
@@ -9,6 +12,7 @@ export default {
   data: function () {
     return {
       engine: null,
+      debug_message: 'hello'
     }
   },
   methods: {
@@ -16,6 +20,7 @@ export default {
       this.engine.resize()
     },
     create_scene: function(engine, canvas){
+      // const vm = this
       // This creates a basic Babylon Scene object (non-mesh)
       const scene = new BABYLON.Scene(engine);
 
@@ -28,35 +33,81 @@ export default {
       camera.attachControl(canvas, true);
 
       // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-      const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
+      // const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
       const point_light_a = new BABYLON.PointLight('point_light_a', new BABYLON.Vector3(0, 3.5, 2.7), scene);
+      point_light_a.diffuse = new BABYLON.Color3(1,1,1);
       const point_light_b = new BABYLON.PointLight('point_light_b', new BABYLON.Vector3(0, 3.5, -2.7), scene);
-      point_light_a.intensity = 0.3
-      point_light_b.intensity = 0.3
+      point_light_a.intensity = 0.7
+      point_light_b.intensity = 0.7
       // Default intensity is 1. Let's dim the light a small amount
-      light.intensity = 0.0;
+      // light.intensity = 0.0;
       const human = BABYLON.MeshBuilder.CreateCylinder('human', {diameter: 0.5, height: 1.8}, scene);
       human.position.x = 2;
       human.position.z = 1.5;
       human.position.y = 0.9;
 
       camera.setTarget(human.position);
+
       const wall_a = BABYLON.MeshBuilder.CreateBox('walls', {height: 4.0, width: 4.6, depth: 0.1});
       const wall_b = BABYLON.MeshBuilder.CreateBox('walls', {height: 4.0, width: 4.6, depth: 0.1});
       const wall_c = BABYLON.MeshBuilder.CreateBox('walls', {height: 4.0, width: 0.1, depth: 10});
       const wall_d = BABYLON.MeshBuilder.CreateBox('walls', {height: 4.0, width: 0.1, depth: 10});
-      wall_a.position.z = 5.0;
+
+      wall_a.position.z =  5.0;
       wall_b.position.z = -5.0;
-      wall_c.position.x = 2.3;
-      wall_d.position.x = -2.3
+      wall_c.position.x =  2.3;
+      wall_d.position.x = -2.3;
 
       wall_a.position.y = 2;
       wall_b.position.y = 2;
       wall_c.position.y = 2;
       wall_d.position.y = 2;
 
+      const move_indicator = BABYLON.MeshBuilder.CreateCylinder("move_indicator", {diameter: 0.2, height: 0.05}, scene);
+      move_indicator.setEnabled(false);
+      move_indicator.position.y = 0.025;
+      const move_indicator_material = new BABYLON.StandardMaterial("move_indicator_material", scene)
+      move_indicator_material.alpha = 0.7
+      move_indicator.material = move_indicator_material
       // Our built-in 'ground' shape.
-      BABYLON.MeshBuilder.CreateGround("ground", {width: 4.6, height: 10.0}, scene);
+      const ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 4.6, height: 10.0}, scene);
+      ground.actionManager = new BABYLON.ActionManager(scene);
+      ground.actionManager.registerAction(
+          new BABYLON.ExecuteCodeAction({trigger: BABYLON.ActionManager.OnPointerOverTrigger},
+              function () {move_indicator.setEnabled(true)}))
+      scene.onPointerObservable.add((pointerInfo) => {
+        if(pointerInfo.type === BABYLON.PointerEventTypes.POINTERMOVE) {
+          move_indicator.position = pointerInfo.pickInfo.pickedPoint;
+        }
+        if(pointerInfo.type === BABYLON.PointerEventTypes.POINTERTAP && move_indicator.isEnabled()){
+          const wall_pos_x = scene.pickWithRay(
+              new BABYLON.Ray(
+                  new BABYLON.Vector3(pointerInfo.pickInfo.pickedPoint.x, camera.position.y, pointerInfo.pickInfo.pickedPoint.z),
+                  new BABYLON.Vector3(1,0,0)))
+          const wall_neg_x = scene.pickWithRay(
+              new BABYLON.Ray(
+                  new BABYLON.Vector3(pointerInfo.pickInfo.pickedPoint.x, camera.position.y, pointerInfo.pickInfo.pickedPoint.z),
+                  new BABYLON.Vector3(-1,0,0)))
+          const wall_pos_z = scene.pickWithRay(
+              new BABYLON.Ray(
+                  new BABYLON.Vector3(pointerInfo.pickInfo.pickedPoint.x, camera.position.y, pointerInfo.pickInfo.pickedPoint.z),
+                  new BABYLON.Vector3(0,0,1)))
+          const wall_neg_z = scene.pickWithRay(
+              new BABYLON.Ray(
+                  new BABYLON.Vector3(pointerInfo.pickInfo.pickedPoint.x, camera.position.y, pointerInfo.pickInfo.pickedPoint.z),
+                  new BABYLON.Vector3(0,0,-1)))
+
+          camera.position.x = Math.max(Math.min(wall_pos_x.pickedPoint.x - 2 * camera.minZ, pointerInfo.pickInfo.pickedPoint.x), wall_neg_x.pickedPoint.x + 2 * camera.minZ)
+          camera.position.z = Math.max(Math.min(wall_pos_z.pickedPoint.z - 2 * camera.minZ, pointerInfo.pickInfo.pickedPoint.z), wall_neg_z.pickedPoint.z + 2 * camera.minZ)
+        }
+      })
+      ground.actionManager.registerAction(
+          new BABYLON.ExecuteCodeAction({trigger: BABYLON.ActionManager.OnPointerOutTrigger},
+              function () {move_indicator.setEnabled(false)}))
+      const ground_material = new BABYLON.StandardMaterial("ground_material", scene)
+      ground_material.diffuseColor = new BABYLON.Color3(0.5, 0.5, 0.5)
+      ground.material = ground_material
+
       return scene;
     }
   },
